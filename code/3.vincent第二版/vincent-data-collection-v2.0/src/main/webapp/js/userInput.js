d5e9b4f5fc80a97d213e1fa3979ee6e821babd8c
Vincent <zzh66688@yeah.net>
2017-08-16 15:18:35 +0800

    hidden the ip and port.

--- code/3.vincent第二版/vincent-data-collection-v2.0/src/main/webapp/js/userInput.js
+++ code/3.vincent第二版/vincent-data-collection-v2.0/src/main/webapp/js/userInput.js
@@ -21,6 +21,6 @@ function oncli(){
 	
 	//通过Image对象请求后端脚本
 	var img = new Image(1, 1);
-	var src = 'http://192.168.21.18:8091/data/dataCollection/log.gif?args=' + encodeURIComponent(args);
+	var src = 'http://192.168.**.***:8091/data/dataCollection/log.gif?args=' + encodeURIComponent(args);
 	img.src = src; 	
 }
